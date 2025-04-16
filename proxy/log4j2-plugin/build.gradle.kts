74148f2b54f81f87df10bb0b1e2bfd64421aa9ad
BezzubickMCPlay <BezzubickMCPlay@outlook.com>
2025-04-16 18:03:09 -0400

    Fix publications

--- proxy/log4j2-plugin/build.gradle.kts
+++ proxy/log4j2-plugin/build.gradle.kts
@@ -1,4 +1,23 @@
+import gradle.kotlin.dsl.accessors._e51b323cfb2e2618e566e57940084292.publishing
+
+plugins {
+    `java-library`
+    `maven-publish`
+    id("velocity-publish")
+}
+
 dependencies {
     implementation(libs.bundles.log4j)
     annotationProcessor(libs.log4j.core)
+}
+
+publishing {
+    publications {
+        create<MavenPublication>("mavenJava") {
+            groupId = project.group as String
+            artifactId = "velocity-proxy-log4j2-plugin"
+            version = project.version as String
+            from(components["java"])
+        }
+    }
 }
